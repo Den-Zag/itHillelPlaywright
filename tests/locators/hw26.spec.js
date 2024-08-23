58b68812882dc69d747e2e638688be3c3cd5201f
denys.zagrebelnyi <denys.zagrebelnyi@mgid.com>
2024-08-23 16:00:36 +0300

    hw30v001

--- tests/locators/hw26.spec.js
+++ tests/locators/hw26.spec.js
@@ -1,6 +1,6 @@
 import {test, expect} from "../../src/fixtures/myFixtures.js";
 
-test.describe('Garage', () => {
+test.describe.skip('Garage', () => {
     test.beforeEach(async ({ garagePage}) => {
         await garagePage.navigate()
     })
